24c988c90754923d77ae14eb45a3f8a0d1a304ff
Jasmin Kaset <kasetski@gmail.com>
2021-02-10 13:48:22 -0600

    initial models

---
+++ raterprojectapi/models/__init__.py
@@ -0,0 +1,7 @@
+from .category import Category
+from .game_category import Game_Category
+from .game import Game
+from .gamer import Gamer
+from .image import Image
+from .rating import Rating
+from .review import Review
