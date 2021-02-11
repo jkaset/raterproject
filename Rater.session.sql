52d9149a533e9a3c04ce427370c4878726402905
Jasmin Kaset <kasetski@gmail.com>
2021-02-11 14:08:09 -0600

    display games, as detail link

---
+++ Rater.session.sql
@@ -0,0 +1,7 @@
+SELECT * FROM auth_user;
+SELECT * FROM authtoken_token;
+SELECT * FROM raterprojectapi_gamer;
+SELECT * FROM raterprojectapi_category;
+SELECT * FROM raterprojectapi_game;
+SELECT * FROM raterprojectapi_review;
+
